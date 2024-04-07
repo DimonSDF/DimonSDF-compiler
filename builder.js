ed30cc817fc40b4643b7e1ae235e155a63803678
Дмитрий Сухих <163548812+DimonSDF@users.noreply.github.com>
2024-04-07 15:51:26 +0300

    Update builder.js

--- builder.js
+++ builder.js
@@ -16,7 +16,7 @@ export function buildDisk(bytes) {
     
     byteCount = bytes.length;
 
-    gameMap.paste(top, 1, 0);
+    gameMap.paste(top, 0, -17);
 
     for (let row = 0; row < byteCount / 4; ++row) {
         let bytes_row = bytes.splice(0, 4);
